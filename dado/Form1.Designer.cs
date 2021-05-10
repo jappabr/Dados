673445d7d33cbfbc0cd92ce4a5132ba9a8dba583
JappaBR <guilherme.dion@gmail.com>
2021-05-10 11:49:00 -0300

    Att Icon

--- dado/Form1.Designer.cs
+++ dado/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Btn1 = new System.Windows.Forms.Button();
             this.Btn2 = new System.Windows.Forms.Button();
             this.Btn3 = new System.Windows.Forms.Button();
@@ -242,6 +243,7 @@
             this.Controls.Add(this.Btn3);
             this.Controls.Add(this.Btn2);
             this.Controls.Add(this.Btn1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Dado";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
